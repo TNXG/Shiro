5f7fb83ee7cd046bb5e39aa791696d67211620ed
TNXG <tianxiang_tnxg@outlook.com>
2024-01-06 02:04:52 +0800

    fix: fix text ,

--- src/components/widgets/post/PostOutdate.tsx
+++ src/components/widgets/post/PostOutdate.tsx
@@ -15,7 +15,7 @@ export const PostOutdate = () => {
     <Banner type="warning" className="my-10">
       <span className="leading-[1.8]">
         这篇文章上次修改于 <RelativeTime date={time} />
-        距今已经过去了 {dayjs().diff(dayjs(time), 'day')} 天
+        ，距今已经过去了 {dayjs().diff(dayjs(time), 'day')} 天
         ，部分内容可能已经不再适用，如有疑问可询问作者。
       </span>
     </Banner>
